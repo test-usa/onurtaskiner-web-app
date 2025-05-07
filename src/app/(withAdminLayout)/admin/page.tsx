eb3498f2495ec9a49d2406ced8412c0024465d4e
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-07 12:01:55 +0600

    setup onurtaskiner frontend boilerplate

---
+++ src/app/(withAdminLayout)/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./dashboard/page";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default AdminPage;
