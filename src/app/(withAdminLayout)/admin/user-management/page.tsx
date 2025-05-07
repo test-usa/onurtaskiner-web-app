3508a31beeec0766c0ad725cafe76013053738e1
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-05-07 17:08:49 +0600

    setup all route

---
+++ src/app/(withAdminLayout)/admin/user-management/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserManagementPage = () => {
+  return (
+    <div>
+      <h1>UserManagementPage</h1>
+    </div>
+  );
+};
+
+export default UserManagementPage;
