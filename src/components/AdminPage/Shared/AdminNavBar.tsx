27046f1ab3808e706f0655a76a4677804eec9083
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-05-08 15:18:52 +0600

    for pull development

--- src/components/AdminPage/Shared/AdminNavBar.tsx
+++ src/components/AdminPage/Shared/AdminNavBar.tsx
@@ -2,7 +2,7 @@ const AdminNavBar = () => {
   return (
     <div className="w-full bg-[var-(--color-bgPrimary)] ">
       <div className="flex items-center justify-between p-4 ">
-        <h1 className="text-xl font-bold">Admin Dashboard</h1>
+        <h1 className="text-xl font-bold">Admin Dashboard here</h1>
         <button className="btn btn-primary">Logout</button>
       </div>
     </div>
