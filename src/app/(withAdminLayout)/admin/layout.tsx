ce7cc8e7c2f0c2cc08dc9230622e326f83601341
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-05-08 14:53:04 +0600

    update side navbar

--- src/app/(withAdminLayout)/admin/layout.tsx
+++ src/app/(withAdminLayout)/admin/layout.tsx
@@ -1,10 +1,10 @@
-// app/admin/layout.tsx
-import AdminSideBar from "@/components/AdminPage/AdminSideBar";
+import AdminNavBar from "@/components/AdminPage/Shared/AdminNavBar";
+import AdminSideBar from "@/components/AdminPage/Shared/AdminSideBar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Gardening Trip Advice - Admin",
-  description: "Admin Dashboard for Gardening Trip Advice",
+  title: " Blinqo- Admin",
+  description: "Admin Dashboard for Blinqo",
 };
 
 export default function AdminLayout({
@@ -13,14 +13,17 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-[#F4F4F4]">
+    <div className="flex flex-col md:flex-row min-h-screen bg-[#F4F4F4]">
       {/* Sidebar */}
-      <div className="w-[20%] bg-[#FFFFFF] text-white">
+      <aside className="w-[20%] bg-white shadow-md z-10">
         <AdminSideBar />
-      </div>
+      </aside>
 
       {/* Main Content */}
-      <div className="w-[80%] bg-base-200 p-1">{children}</div>
+      <main className="w-[80%] flex-1 overflow-hidden bg-base-200 mt-10 md:mt-0">
+        {/* <AdminNavBar /> */}
+        <div className="p-4">{children}</div>
+      </main>
     </div>
   );
 }
