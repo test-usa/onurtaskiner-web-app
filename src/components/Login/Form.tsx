e3716bff5f75f2407f6e4605035610e0749b1189
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-16 10:12:05 +0600

    remove promise fn

--- src/components/Login/Form.tsx
+++ src/components/Login/Form.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
 import { Button } from "../ui/button";
 
 const LoginForm = () => {
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log("Hello world", email, password);
+  };
   return (
     <div className="w-full bg-white font-Robot">
       <div className="flex items-center justify-center h-screen p-3">
@@ -9,13 +17,14 @@ const LoginForm = () => {
           <h1 className="text-4xl sm:text-5xl font-semibold text-center text-[var(--color-secondary)]">
             Admin Login
           </h1>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleLogin}>
             <div className="flex flex-col space-y-3">
               <label className="text-sm sm:text-[17px] text-[var(--color-textPrimary)]">
                 Email
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="Enter your mail"
                 className="py-2 px-5 sm:py-2.5 sm:px-6 rounded-[12px] border-2"
               />
@@ -26,6 +35,7 @@ const LoginForm = () => {
                 Password
               </label>
               <input
+                name="password"
                 type="password"
                 placeholder="Enter your password"
                 className="py-2 px-5 sm:py-2.5 sm:px-6 rounded-[12px] border-2"
